d3d79b7532075a5d9e5dd2ec599537d77a083c6c
DominicT1995 <domedt@gmail.com>
2024-03-06 23:40:24 -0600

    updated readme and comments added to app.py code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SQLAlchemy-Challenge
 UTSA Data Analytics Bootcamp SQLAlchemy challenge containing .sqlite files and SQL ORM script used to analyze and output the data to visual figures and an app.
+
+------------------------------------------------------------------------------------------------------------------
+SURFSUP
+
+Inside the SQLAlchemy-Challenge Repository there is a Resources folder used to supply the hawaii_measurements.csv and hawaii_stations.csv that make up the hawaii.sqlite database that is used for analysis in the climate_analysis.ipynb file and for API creation with the app.py. 
+
+------------------------------------------------------------------------------------------------------------------
+climate_analysis.ipynb
+
+The climate_analysis jupyter notebook takes creates an engine from the hawaii.sqlite file and opens a session to perform multiple analytical queries on the data. Queries include filtering climate data such as daily precipitation and temperature in hawaii based upon recordings made within in the past year and by recordings made by unique weather stations. The data is then converted to a pandas dataframe for data visualization of precipitation and temperature over the most recent year available.
+
+------------------------------------------------------------------------------------------------------------------
+app.py
+
+The app.py file serves to create a server API by utilizing Flask and engine sessions from the hawaii.sqlite database. The app defines multiple routes including the homne route displaying all available routes and formatting notes for inputs, the precipitation route that outputs precipitaion recordings for every day in the most recent year, the stations route which outputs all stations in the database, the tobs route which displays dates and temperatures for all recordings in the past year of the most active station, and the start and start-end routes which take in user route date input in order to output the minimum, maximum, and average temperatures for the date ranges supplied based on start date and start and end date ranges.
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -38,6 +38,8 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+# Establish a home route displaying available routes in the API
+# as well as formatting instructions for start and start-end routes
 @app.route("/")
 def home():
     return (
@@ -52,17 +54,23 @@ def home():
         f"Dates for &ltstart&gt and &ltend&gt should be formatted as mm-dd-yyyy or mmddyyyy."
     )
 
+# Create the precipitation route displaying dates and rainfall levels
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
+    # Obtain date one year prior to final oberserbed date
     query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
+    # Query for date and rainfall value filtered by desired date and ordered by date
     year_prec = session.query(measure.date, measure.prcp).filter(measure.date >= query_date).order_by(measure.date).all()
 
     session.close()
 
+    # Use list comprehension to replace all null None values with 0.0
     prec_list = [(item[0], 0.0) if item[1] == None else item for item in year_prec]
 
+    # Initialize and iterate through the data list to create an output list of dictionaries
+    # Utilize indexing with x variable set as date to obtain multiple recordings for same dates
     prec_dict = {}
     item_list = []
     x = prec_list[0][0]
@@ -83,31 +91,40 @@ def precipitation():
 
     prec_dict[x] = item_list
 
+    # Return a json of our precipitation, date data at given app route
     return jsonify(prec_dict)
 
+# Create the stations route displaying all stations in the database
 @app.route("/api/v1.0/stations")
 def stations():
 
+    # Query to obtain all stations in database ordered by most active to least
     station_list = session.query(measure.station).\
         group_by(measure.station).order_by(func.count(measure.station).desc()).all()
 
     session.close()
 
+    # Return a json of our stations at given app route
     return jsonify(list(np.ravel(station_list)))
 
+# Create the tobs route displaying temperatures and dates for most recent recorded year of most active station
 @app.route("/api/v1.0/tobs")
 def tobs():
 
+    # Obtain date one year prior to final oberserbed date
     temp_query_date = dt.date(2017, 8, 18) - dt.timedelta(days=365)
 
+    # Query to obtain the most active station in the database
     active_station = session.query(measure.station).\
         group_by(measure.station).order_by(func.count(measure.station).desc()).first()
 
+    # Query to obtain dates and temperatures filtered by desired date and defined active station
     temp_data = session.query(measure.date, measure.tobs)\
         .filter(measure.date >= temp_query_date).filter(measure.station == active_station[0]).all()
 
     session.close()
 
+    # Iterate through query and obtain key value pairs for date and temperature and append to empty list
     all_temps = []
     for date, tobs in temp_data:
         temp_dict = {}
@@ -115,28 +132,37 @@ def tobs():
         temp_dict["temperature"] = tobs
         all_temps.append(temp_dict)
 
+    # Return json of our dates and temperatures to the given app route
     return jsonify(all_temps)
 
+# Create the start route that gives temperature statistics on given date range based on start date to end of database
 @app.route("/api/v1.0/start/<start>")
 def start_date(start):
 
+    # Canonicalize date input to take dashed date format
     canon = start.replace("-", "")
 
+    # Slice date input based on string location and convert to integer type
     mm = int(canon[slice(0, 2)])
     dd = int(canon[slice(2, 4)])
     yyyy = int(canon[slice(4, 8)])
 
+    # Convert slices to dt.date format
     user_start_input = dt.date(yyyy, mm, dd)
 
+    # Query and convert to list displaying all dates in database
     db_check = list(np.ravel(session.query(measure.date).all()))
 
+    # Reformat and check if user given date occurs in our database
     if user_start_input.strftime("%Y-%m-%d") in db_check:
 
+        # If date present, continue to query min, max, avg temperature based on start to end date of database
         start_search = session.query(func.min(measure.tobs), func.max(measure.tobs), func.avg(measure.tobs)).\
             filter(measure.date >= user_start_input).all()
         
         session.close()
 
+        # Convert query to key value pairs for visualization in API
         start_results = []
         for stat in start_search:
             start_dict = {}
@@ -145,20 +171,25 @@ def start_date(start):
             start_dict["avg"] = stat[2]
             start_results.append(start_dict)
 
+        # Return json of temperature stats based on given start date to end of database to the given app route
         return jsonify(start_results)
     
+    # If date is not present in the database, return an error statement
     else:
         
         session.close()
 
         return jsonify({"error": f"Date: {start} not found in database."}), 404
 
+# Create the start-end route that gives temperature statistics on given date range based on start and end date supplied
 @app.route("/api/v1.0/start-end/<start>/<end>")
 def start_end_date(start, end):
 
+    # Canonicalize date inputs to take dashed date format
     canon_start = start.replace("-", "")
     canon_end = end.replace("-", "")
 
+    # Slice date inputs based on string location and convert to integer type
     mm_st = int(canon_start[slice(0, 2)])
     dd_st = int(canon_start[slice(2, 4)])
     yyyy_st = int(canon_start[slice(4, 8)])
@@ -167,18 +198,23 @@ def start_end_date(start, end):
     dd_ed = int(canon_end[slice(2, 4)])
     yyyy_ed = int(canon_end[slice(4, 8)])
 
+    # Convert slices to dt.date format
     user_start_input = dt.date(yyyy_st, mm_st, dd_st)
     user_end_input = dt.date(yyyy_ed, mm_ed, dd_ed)
 
+    # Query and convert to list displaying all dates in database
     db_check = list(np.ravel(session.query(measure.date).all()))
 
+    # Reformat and check if user given dates occur in our database and that start date occurs befoere end date
     if (user_start_input.strftime("%Y-%m-%d") and user_end_input.strftime("%Y-%m-%d") in db_check) and (user_end_input > user_start_input):
 
+        # If conditions met, continue to query min, max, avg temperature based on start and end dates given
         dates_search = session.query(func.min(measure.tobs), func.max(measure.tobs), func.avg(measure.tobs)).\
             filter(measure.date >= user_start_input, measure.date <= user_end_input).all()
         
         session.close()
 
+        # Convert query to key value pairs for visualization in API
         dates_results = []
         for stat in dates_search:
             dates_dict = {}
@@ -187,19 +223,23 @@ def start_end_date(start, end):
             dates_dict["avg"] = stat[2]
             dates_results.append(dates_dict)
 
+        # Return json of temperature stats based on given start and end dates to the given app route
         return jsonify(dates_results)
     
+    # If date is not present in the database, return a specific error statement defining reason for error
     elif user_start_input.strftime("%Y-%m-%d") and user_end_input.strftime("%Y-%m-%d") not in db_check:
         
         session.close()
 
         return jsonify({"error": f"Date: '{start}' or '{end}' not found in database."}), 404
     
+    # If  start date is not before end date, return a specific error statement defining reason for error
     elif user_end_input < user_start_input:
 
         session.close()
 
         return jsonify({"error": f"start date '{start}' is later than end date '{end}', null values returned."}), 404
 
+# Run debugger for app
 if __name__ == "__main__":
     app.run(debug=True)
